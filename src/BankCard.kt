74168149a196f851e683a6f539445e6439487401
DJAlexRus <akobelev18@gmail.com>
2025-01-11 17:34:29 +0500

    Add files via upload

--- src/BankCard.kt
+++ src/BankCard.kt
@@ -1,2 +1,31 @@
-class BankCard {
+class BankCard(val numberCard:String,private val cardCode:Int,
+               private val passCard:String) {
+
+    var pass = ""
+        set(value) {
+            field=when{
+                value == passCard ->  value
+                else -> "false"
+            }
+        }
+
+    fun checkPass ():Boolean{
+        var booleanNext=true
+        println("Для вывода кода карты введите пароль:")
+        var countPass=3
+        while (countPass-- > 0) {
+            pass = readln()
+            if (pass !="false"){
+                println("Код вашей карты: $cardCode")
+                countPass =0
+                booleanNext=true
+            }else{
+                println("Пароль не верный")
+                println("Попробуйте еще раз ввести пароль, осталось попыток: $countPass")
+                booleanNext=false
+                if (countPass==0) println("Извините, карта заблокирована")
+            }
+        }
+        return booleanNext
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+// Задание 1
+    val cardOne=BankCard("2165 5485 6854 6895",758, "1111")
+    var booleanNext = true
+    do{
+        println("Введите значение:\n \t1 - Показать номер карты\n " +
+                "\t2 - Показать код карты \n \t0 - Выход")
+        val input = readln().toString()
+        if (input =="0") {
+            println("Вы вышли из программы.")
+            booleanNext = false
+            break
+        } else if (input =="1") {
+            println("Ваш номер карты: ${cardOne.numberCard}")
+            println()
+        } else if (input =="2"){
+            booleanNext = cardOne.checkPass()
+            println()
+        }else{
+            println("Запрос не понятен, попробуйте еще раз")
+            booleanNext=true
+            continue
+        }
+    }while (booleanNext)
+    println("Всего доброго.")
+//Задание 2
+    println()
+    var a=7
+    a += 7
+    println("a+7=$a")
+    a -= 4
+    println("a-4=$a")
+    a *= 2
+    println("a*2=$a")
+    a %= 3
+    println("Остаток от деления на 3=$a")
+//Задание 3
+    println()
+    var number = 753
+    var count = 1
+    repeat(5){
+        println("Результат$count: $number -> ${reversNum(number)}")
+        number = (10..190).random()
+        count++
     }
+}
+fun reversNum (aNum:Int):Int{
+    val charArr=aNum.toString().toCharArray()
+    val charTemp = charArr[0]
+    charArr[0] = charArr[charArr.size-1]
+    charArr[charArr.size-1] = charTemp
+    return charArr.concatToString().toInt()
 }
